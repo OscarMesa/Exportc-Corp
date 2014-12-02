5bd6b31afe2b4212c45b88a9248c9eef674cce4b
Julian Giraldo <julian.giraldo8@gmail.com>
2014-12-01 22:18:20 -0500

    completo

---
+++ protected/views/site/login_modal.php
@@ -0,0 +1,31 @@
+<div class="cd-user-modal"> <!-- this is the entire modal form, including the background -->
+    <div class="cd-user-modal-container"> <!-- this is the container wrapper -->
+
+        <div id="cd-login"> <!-- log in form -->
+            <form class="cd-form">
+                <p class="fieldset">
+                    <label class="image-replace cd-username" for="signin-email">E-mail</label>
+                    <input class="full-width has-padding has-border" id="signin-username" type="text" placeholder="">
+                    <span class="cd-error-message">Error message here!</span>
+                </p>
+
+                <p class="fieldset">
+                    <label class="image-replace cd-password" for="signin-password">Password</label>
+                    <input class="full-width has-padding has-border" id="signin-password" type="password"  placeholder="">
+                    <!--                            <a href="#0" class="hide-password">Hide</a>-->
+                    <span class="cd-error-message">Error message here!</span>
+                </p>
+
+                <p class="fieldset">
+                    <input type="checkbox" id="remember-me" checked>
+                    <label for="remember-me">Remember me</label>
+                </p>
+
+                <p class="fieldset">
+                    <input class="full-width" type="submit" value="Login">
+                </p>
+            </form>
+            <a href="#0" class="cd-close-form">Close</a> 
+        </div> <!-- cd-login -->
+    </div> <!-- cd-user-modal-container -->
+</div> <!-- cd-user-modal -->

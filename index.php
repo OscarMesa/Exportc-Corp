ab975489c474bb15c051212c3e52b1416db5c8d0
Calcifer <Calcifer@Calcifer-HP>
2014-12-20 11:14:15 -0600

    script

--- index.php
+++ index.php
@@ -1,8 +1,61 @@
-<?php
+<!DOCTYPE HTML>
+</html>
+<?php 
+     $user ="root";
+     $pass = "";
+     $server="localhost";
+     $db ="tablatarifas";
+     $conn = mysql_connect($server,$user,$pass) or die ("Error al conectarse".mysql_error());
+     mysql_select_db($db);           
+ ?>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+  
+    <form method='POST' name='formselect' action=''>
+      <select name="envio">     
+             <option value='1' name='envio' onchange='submit()'>
+                  correo                  
+             </option>
+             <option value='2'>
+                  carga                  
+             </option>
 
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+        </select><br/>
+       <select name="origene" onchange='Submit()'>     
+             <option>
+                  AREA - METROPOLITANA
+             </option>
+        </select><br/>
+        <select name="destino" onchange='submit()'>     
+             <?php   
+                                
+                    $sql = "SELECT * FROM CostoEnvio order by destino"; 
+                    $query = mysql_query($sql);              
+                    while ($row=mysql_fetch_array($query)) {                                       
+                       echo "<option value='".$row['sobre']."'"; 
+                       if($_POST['destino']==$row['sobre'])  
+                       echo "selected";
+                       echo ">";                                         
+                       echo $row['destino'];
+                       echo '</option>'; 
+                     }          
+                    
+              ?>
+        </select>
+       <input type='txt' name='txtcarga' valua='submit()'>
+   </form>
+    <?php 
+       $resdestino = $_POST['destino'];
+       $txt = $_POST['txtcarga'];
+       echo "envio ".$resdestino * $txt;
+      /* if($_POST['txtcarga'] < 30)
+        echo "valor minimo bd";
+       else
+         ($_POST['txtcarga'] * -30)* $_POST['kilo'] + $_POST['minimogk'];*/
+     ?>
+</body>
+</html>
 
---
+++ utilidades/Conexion.php
@@ -0,0 +1,21 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * Description of Conexion
+ *
+ * @author Calcifer
+ */
+class Conexion extends PDO{
+    //put your code here
+    
+     
+    
+    
+    
+}
